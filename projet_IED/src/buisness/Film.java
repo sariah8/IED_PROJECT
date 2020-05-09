aac91b3ccef6e11fbe68d36765c6b2bbb17e85ea
Gabriel <Gabriel@PC-Gab.home>
2020-05-09 15:15:31 +0200

    voila les noms des attributs de la class film

--- projet_IED/src/buisness/Film.java
+++ projet_IED/src/buisness/Film.java
@@ -1,16 +1,23 @@
 package buisness;
 
+import java.util.ArrayList;
+
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class Film {
 
 	private String title;
-	private String released;
+	private String releaseDate;
 	private String genre;
 	private String distributor;
 	private double budget;
 	private double domesticGross;
 	private double worldwideGross;
+	private String realisator;
+	private String director;
+	private ArrayList<String> actors;
+	private ArrayList<String> producers;
+	private String resume;
 	
 }
