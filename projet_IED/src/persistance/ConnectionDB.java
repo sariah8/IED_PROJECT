b55b65e9b445d42bc36d7ca7248d849bf9b05b6e
Sariah <Sariah@DESKTOP-1FOT9K7>
2020-05-10 18:58:24 +0200

    JDBC connection

--- projet_IED/src/persistance/ConnectionDB.java
+++ projet_IED/src/persistance/ConnectionDB.java
@@ -1,20 +1,26 @@
 package persistance;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConnectionDB {
 
 	private static String host = "localhost";
-	private static String base = "ied";
+	private static String base = "projet_ied";
 	private static String user = "root";
 	private static String password = "";
-	private static String url = "jdbc:mysql://" + host + "/" + base;
-
+	private static String url = "jdbc:mysql://" + host +":3306" + "/" + base;
 	/**
 	 * Lazy singleton instance.
 	 */
 	private static Connection connection;
+	public ConnectionDB con = new ConnectionDB();
+	
+	public static void main(String[] args) throws Exception, IOException {
+		getConnection();
+		
+	}
  
 	//Local Connection
 	public static Connection getConnection() {
